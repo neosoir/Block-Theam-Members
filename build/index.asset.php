adeb588f868b8a649131d5f675d96c0679d9d468
Neo Soir <leonard@neosoir.com>
2022-11-25 11:09:28 -0600

    [add] rename plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0baa3ef58a23fefbf606');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ece376a4eaab1d5f72bb');
