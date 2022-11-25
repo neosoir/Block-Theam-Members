5b7114d7e89a05d81d31103d242139cc1c45fe6d
Neo Soir <leonard@neosoir.com>
2022-11-25 16:12:56 -0600

    [add] three member by defaul

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f89f3f74d14637a88745');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '59f2267ae2103c855c16');
