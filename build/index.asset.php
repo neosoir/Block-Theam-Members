c134b3f27ee5c1f1eb2f6872b69fe73951f481ef
Neo Soir <leonard@neosoir.com>
2022-11-25 18:41:11 -0600

    [add] columns css for frontend

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4ae616213e299dadc378');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '1b65716594cc261f1ecf');
