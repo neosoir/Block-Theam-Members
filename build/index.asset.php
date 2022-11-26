be96bbf0582b1b3f34b6d1b9f4c1517ab4ebb146
Neo Soir <leonard@neosoir.com>
2022-11-26 11:40:56 -0600

    [add] image basic css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a3d208509965f794a8c5');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8fe602a44bb83271b36f');
