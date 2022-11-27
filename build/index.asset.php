18a059d05b72a1381e953941ae99f0388971e741
Neo Soir <leonard@neosoir.com>
2022-11-26 20:43:37 -0600

    [add] style for the social buttons

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '77606c7368bb4d8a7496');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6f756526b4899466ded1');
